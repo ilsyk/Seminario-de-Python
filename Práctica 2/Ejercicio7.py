bebb4c1e1ae54ccbe5cc16efcbcd6e6ba9a842cc
Alejo <tmykz.01@gmail.com>
2022-04-12 00:10:46 -0300

    Ejercicio 7 mejorado

--- Práctica 2/Ejercicio7.py
+++ Práctica 2/Ejercicio7.py
@@ -1,17 +1,10 @@
 import string
 from collections import Counter
 
-cadena = input("Ingrese cadena o frase: ")
-
-"""letras = set(string.ascii_letters)
-print(letras)
-"""
+cadena = input("Ingrese una palabra o frase: ")
 apariciones = Counter(cadena.lower()).most_common()
-# apariciones = lista de tuplas con cada letra y cantidad de veces que aparece
-# dicha letra en la cadena o frase
-print(apariciones)
-apariciones2 = Counter("a")
-for i in apariciones:
-	if i[0] in string.ascii_lowercase and i[1] > 1:
-		print("Esta cadena o frase no es un holograma")
-		break;
+
+if apariciones[0][1] > 1:
+	print(f"'{cadena}' no es un holograma")
+else:
+	print(f"'{cadena}' es un holograma")
